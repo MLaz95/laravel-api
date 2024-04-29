d8bea657dbe66af4fd77d1f8acdc41b87fde8448
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-04-29 16:57:28 +0200

    first commit

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for($i = 0; $i < 10; $i++){
+            $newProject = new Project();
+            $newProject->name = $faker->word();
+            $newProject->description = $faker->paragraph();
+            $newProject->github = $faker->url();
+            $newProject->save();
+        }
+    }
+}
