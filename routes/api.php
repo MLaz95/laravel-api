945125bc741e5ac6cc1e13c61ce67f5967c7aa49
emanuele-pogliari <manu.pogliari@gmail.com>
2024-05-03 12:55:37 +0200

    fix: aggiunto rotta per i nuovi contatti nell'api controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,3 +26,5 @@ Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{slug}', [ProjectController::class, 'show']);
 
 Route::get('/projects/search/{query}', [ProjectController::class, 'search']);
+
+Route::post('/new-contact', [LeadController::class, 'store']);
